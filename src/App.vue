2bef4272e5754377e3a6d0011eaceb492380cbe9
13936252133 <ls57765867@126.com>
2023-04-03 16:05:08 +0800

    数字驾驶舱初始化

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <router-view></router-view>
+</template>
+
+<script setup lang="ts">
+import {useBaseStore} from './store'
+import {storeToRefs} from 'pinia'
+
+const state = reactive({test: 'test'})
+const {name, age, doubleAge} = storeToRefs(useBaseStore())
+const store = useBaseStore()
+
+const editName = () => {
+  store.setName()
+}
+const editAge = () => {
+  age.value++
+}
+</script>
